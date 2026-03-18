fc33022877fbfb183ef4af8204881cf68af695f2
Tabitha <tabithaclitus@gmail.com>
2026-03-18 04:48:45 +0000

    added

--- autopush.sh
+++ autopush.sh
@@ -1,8 +1,12 @@
+echo "What did you change? (type your commit message)"
+read message
 echo "Adding all changes..."
 git add .
 echo "Committing..."
-git commit -m "auto commit"
+git commit -m "$message"
 echo "Pushing to GitHub..."
 git push
 echo "Done! Code is on GitHub!"
+
+
 
