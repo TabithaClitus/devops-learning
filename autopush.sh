c7e0e9c57e625c4826310574db75d54b4b86ac5a
Tabitha <tabithaclitus@gmail.com>
2026-03-18 04:35:36 +0000

    auto commit

---
+++ autopush.sh
@@ -0,0 +1,8 @@
+echo "Adding all changes..."
+git add .
+echo "Committing..."
+git commit -m "auto commit"
+echo "Pushing to GitHub..."
+git push
+echo "Done! Code is on GitHub!"
+
---
+++ myfirst.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hello, Tabitha!"
+echo "Today you are learning Bash scripting!"
+echo "Your current folder is:"
+pwd
+echo "Your files are:"
+ls
+
